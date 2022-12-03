7798eb7fdb1bedb858c4c937b11b8388bc628139
VickoFlores <82225605+VickoFlores@users.noreply.github.com>
2022-12-02 21:53:59 -0300

    Add files via upload

---
+++ Head.php
@@ -0,0 +1,65 @@
+<!--
+<!DOCTYPE html>
+<html>
+        <head>
+            <meta charset="utf-8">
+            <meta name="viewport" content="width=device-width, user-scalabe=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+            
+            
+            <script src="https://kit.fontawesome.com/bb549b9791.js" crossorigin="anonymous"></script>
+            
+            <link rel="stylesheet" type="text/css" href="CSS/Estilos.css">
+            <link rel="stylesheet" type="text/css" href="CSS/Fuentes.css">
+            <link rel="stylesheet" type="text/css" href="CSS/Animaciones.css">
+
+           
+        </head>
+
+        <body>
+            <div class="Contenedor">
+                <header>
+                    <ul class="MenuM">
+                        <li id="home"><h1>#Home{</h1><i class="fas fa-bars" id="menu"></i>
+                            <ul id="SubM">
+                                <li id="demo">.Demos
+                                    <ul id="SubD">
+                                        <li>{One Page</li>
+                                        <li>MultiPage</li>
+                                        <li>Database}</li>
+                                    </ul>
+                                </li>
+                                <li>Experiencia</li>
+                                <li>Proyectos</li>
+                                <li>Calcular Presupuesto</li>
+                                <li>Sobre Mí}</li>
+                            </ul>
+                        </li>
+                    </ul>
+                </header> -->
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Portafolio</title>
+    <link rel="stylesheet" type="text/css" href="CSS/Fuentes.css">
+    <link rel="stylesheet" type="text/css" href="CSS/Estilos1.css">
+    <link rel="stylesheet" type="text/css" href="CSS/Menu1.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
+</head>
+<body>
+    <div class="Contenedor">
+        <header>
+            <ul class="MenuD">
+                <li id="Home"><div>Inicio</div>
+                    <ul id="SubMenuD">
+                        <li>Demos</li>
+                        <li>Experiencia</li>
+                        <li>Contacto</li>
+                    </ul>
+                </li>
+            </ul>
+        </header>
+    </div>
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,65 @@
+<?php include_once 'Includes/Head.php'?>
+
+ <!--               <main>
+                    <div id="sol"></div>
+                    <div id="farm"></div>
+                    <div id="cloud">
+                        <img src="IMGS/Diseño_sin_título-removebg-preview.png">
+                    </div>
+                    <section id="intro" class="sec">
+                        <h2>Victoria Flores();</h2>
+                        <p>Programadora Front-End, Edición de Fotografía, 
+                            Mantenimiento de Sitios Webs, Hosting y Dominio.<i id="terminal" class="fas fa-terminal"></i></p>
+                        
+                        <img src="Animaciones/Nerón-Inicio/Nerón-Inicio.gif" width="100%">
+                    </section>
+                    <section id="demos" class="sec">
+                        <h2>Demos</h2>
+                    </section>
+
+                    <section id="exp" class="sec">
+                        <h2>Experiencia</h2>
+                        <div>
+                            <iframe src="https://robertosuarez.webcindario.com/"></iframe>
+                            <h3>Portafolio</h3>
+                            <p>Primer trabajo, en el que consistía diseñar una web sencilla y rápida para el señor Roberto, 
+                                demostrando sus habilidades, capacidad y conocimiento acerca de las áreas de trabajo, como la plomería, electricidad, albañilería y carpintería.</p>
+                        </div>
+                        <div>
+                            <iframe src="https://mamucha.netlify.app/"></iframe>
+                            <h3>Trabajo Final del Curso de Programación(2020)</h3>
+                        </div>
+                    </section>
+
+                    <section class="sec" id="proyec">
+                        <h2>Proyectos</h2>
+                        <div>
+                            <h3>Estoy Trabajando en...</h3>
+                            <img src="IMGS/concept-g9e6c735a6_640.jpg">
+                        </div>
+                        <div>
+                            <h3>Mis Planes son...</h3>
+                            <img src="IMGS/concept-g9e6c735a6_640.jpg">
+                        </div>
+                    </section>
+                    <section id="Calcu" class="sec">
+                        <h2>Calcular Presupuesto</h2>
+                    </section>
+                    <section id="Me" class="sec">
+                        <h2>Sobre Mí</h2>
+                        <p>
+                            Mi nombre es Victoria Flores, programo desde los 14 años, en el 2020 tomé un curso de programación 
+                            en donde he reforzado mis conocimientos sobre el mundo del desarrollo Front-End.
+                        </p>
+                        <p>
+                            Mi proyecto final de curso es el blog "La Pastelería de Mamucha".
+                            Soy muy responsable con mi trabajo, jamás me compremetería en algo que no excede mis capacidades, 
+                            y me destacó por ser muy detallista, haciendo que el trabajo sea perfecto para mi cliente.
+                        </p>
+                        <h3>Lenguajes</h3>
+
+                    </section>
+                </main>
+            </div>
+        </body>
+</html>-->
